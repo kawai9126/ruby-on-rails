fe161845589b4537f6d1ed58f7c9c2733a37147c
EC2 Default User <ec2-user@ip-172-31-0-71.ec2.internal>
2021-05-04 12:48:26 +0000

    [strat]rails-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'a3f0d9187b788bde7974933d25cc4876b52d73f432a1bc623fcbf10ab77e471063230b53e1c08c17a4164bf3ee0a303916d0cc139220410c31b3301cd20c1c03'
