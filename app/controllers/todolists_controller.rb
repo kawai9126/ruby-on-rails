4ac5d4f3533b3f3563302761d37f88232c3e8369
kawai9126 <iroha.9126@gmail.com>
2021-05-29 10:06:02 +0000

    egui

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,33 +1,36 @@
 class TodolistsController < ApplicationController
   def new
-     @list = List.new
+    @list = List.new
   end
+
   def create
-      list = List.new(list_params)
-      list.save
-      redirect_to todolist_path(list.id)
+    list = List.new(list_params)
+    list.save
+    redirect_to todolist_path(list.id)
   end
 
   def index
-      @lists = List.all
+    @lists = List.all
   end
 
   def show
-     @list = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
     @list = List.find(params[:id])
   end
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
+
   def destroy
-      list = List.find(params[:id])  # データ（レコード）を1件取得
-    list.destroy  # データ（レコード）を削除
-    redirect_to todolists_path  # 投稿一覧画面へリダイレクト
+    list = List.find(params[:id]) # データ（レコード）を1件取得
+    list.destroy # データ（レコード）を削除
+    redirect_to todolists_path # 投稿一覧画面へリダイレクト
   end
 
   private
